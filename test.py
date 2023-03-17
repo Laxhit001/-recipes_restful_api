523f0fa7ac71104bb8c694f77201640c01fe1460
Laxhit001 <86069390+Laxhit001@users.noreply.github.com>
2023-03-17 23:36:51 +0530

    update test

--- test.py
+++ test.py
@@ -12,7 +12,6 @@ BASE = "http://127.0.0.1:5000/"
     #'image': 'http://nutricionenlared.com/web/wp-content/uploads/2014/03/broccoli-feta-frittata-l-400x300.jpg'
     #})
 #response = requests.delete(BASE + "recipes/souffle")
-
 response = requests.patch(BASE + "recipes/souffle", {
     'process': 'Precalentar horno. Hervir el brocoli. Rehogar cebolla y morron. Batir huevos con quesos. Mezclar todo y al horno 20 minutos.'
 })
